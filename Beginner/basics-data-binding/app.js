e7cf6fb93d7b91d21a54185c01b499cfe0e74421
TheKaizenWolf <raul670saavedra@gmail.com>
2020-09-27 17:45:59 -0300

    update

--- Beginner/basics-data-binding/app.js
+++ Beginner/basics-data-binding/app.js
@@ -3,22 +3,22 @@ const app = Vue.createApp({
     return {
       name: 'Raul',
       age: 20,
-      imageUrl: 'https://images.unsplash.com/photo-1529257414772-1960b7bea4eb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
-    }
+      imageUrl:
+        'https://images.unsplash.com/photo-1529257414772-1960b7bea4eb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    };
   },
   methods: {
     age5() {
       return this.age + 5;
     },
     favoriteNumber() {
-      const number = Math.random()
+      const number = Math.random();
       if (number < 0.5) {
-        return 0
-      } else {
-        return 1
+        return 0;
       }
-    }
-  }
-})
+      return 1;
+    },
+  },
+});
 
-app.mount('#user-goal')
+app.mount('#user-goal');
